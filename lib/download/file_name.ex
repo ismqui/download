fc153ef87f8252bafd71db6ffe1f986d1b356aab
ismqui <ismqui.git@gmail.com>
2018-01-29 19:11:24 +0100

    add more docs

--- lib/download/file_name.ex
+++ lib/download/file_name.ex
@@ -4,15 +4,24 @@ defmodule Download.FileName do
     Module contains functions to download files from an url.
   """
 
+  @doc """
+  Downloads the body or the url and saves it in the directory with the name
+  given in name.
+  """
+  @spec get_file(String.t, String.t) :: any
   def get_file(url, name) do
     download_file(HTTPoison.get(url), name)
   end
 
+  @type poison_error :: %HTTPoison.Error{reason: String.t}
+  @spec download_file({atom, poison_error}, any) :: {atom, String.t}
   def download_file({:error, %HTTPoison.Error{reason: reason}}, _name) do
     IO.puts "Error, reason: #{reason}"
     {:error, reason}
   end
 
+  @type poison_resp :: %HTTPoison.Response{body: String.t}
+  @spec download_file({atom, poison_resp}, String.t) :: any
   def download_file({:ok, %HTTPoison.Response{body: body}}, name) do
     name
     |> File.write(body)
@@ -20,12 +29,12 @@ defmodule Download.FileName do
   end
 
   def save_file(:ok) do
-    IO.puts "Todo ok"
+    IO.puts "Save ok"
     :ok
   end
 
   def save_file(result = {:error, reason}) do
-    IO.puts "Something went wrong reason: #{reason}"
+    IO.puts "Something went wrong saving file reason: #{reason}"
     result
   end
 
